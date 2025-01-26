8451783d3079a2387ba313635f84584c3f44f3e5
DDOLDDOL <doyt@naver.com>
2025-01-27 05:36:05 +0900

    style: Remove unused files

--- lib/chat/cubits/message_update_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'message_update_cubit.dart';
-
-@freezed
-class MessageUpdateState with _$MessageUpdateState {
-  const factory MessageUpdateState({
-    @Default([]) List<ChattingMessage> newMessages,
-  }) = _MessageUpdateState;
-}
